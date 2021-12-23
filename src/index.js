bc6f5b6abc5f93d4c783ac9919249c2d609ed765
skiran017 <skiran017@gmail.com>
2021-12-23 20:53:38 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import App from './App';
 import GlobalStyle from './styles/GlobalStyles';
 
 ReactDOM.render(
   <React.StrictMode>
     <GlobalStyle />
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
